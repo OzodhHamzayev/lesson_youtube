bef3b69d9249d0d0accd13c89b5364e401795ae3
Ozod Hamzayev <ozodhamzayev10@gmail.com>
2025-04-13 19:11:02 +0500

    model

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,6 +20,8 @@ class AdminController extends Controller
     {
         $this->adminServices = $adminServices;
     }
-    public function asd(){}
+    public function index(){
+        
+    }
 
 }
